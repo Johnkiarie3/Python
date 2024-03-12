da0aa8996ab15121a392f6b7750c35419da29a67
Johnkiarie3 <kiariejohnmaxwell5@gmail.com>
2024-03-12 13:39:25 +0300

    Updates

--- pythonProject/python_intro.py
+++ pythonProject/python_intro.py
@@ -37,3 +37,73 @@ book_price_123= 40 #valid variable naming
 
 x=y=z=10 #assigning one value to multiple variables
 x,y,z=30,40,50 #multiple values being assigned to multiple variables
+
+
+#casting example 1
+price= 10
+quantity= 5
+total= price*quantity
+print("the total is:"+str(total)+ "Kenyan shillings")
+
+#casting example 2
+firstname= "Jonathan "
+secondname= "Allan"
+thirdname= firstname+secondname
+print("His thirdname name is:" +thirdname)
+
+#casting example 3
+firstname= "Jonathan "
+secondname=73734
+thirdname=firstname+str(secondname)
+print("His thirdname name is:" ,thirdname)
+
+#casting example 4
+a= 1
+b=2
+c= a+b
+print("the sum of a and b is:",c)
+
+#ASSIGNMENT OPERTAORS
+#division
+print(7.5/1.5)
+#floor division
+print(7.5//1.5)
+#modulus
+print(3%7)
+#exponentiation
+print(2**5)
+
+#ASSIGNMENT OPERATORS
+x=5
+print(x) #output is 5
+x+=5 #x=X+5
+print(x) #output is 10
+x-=4 #x=x-4
+print(x) #output is 6
+x*=3 #x=x*3
+print(x) #output is 18
+x/=2 #x=x/2
+print(x) #output is 9
+
+#COMPARISON OPERATORS
+a=20
+b=20
+print(a==b) #output is TRUE
+
+a=20
+b=28
+print(a!=b) #output is TRUE
+
+#LOGICAL OPERATORS
+age= 20
+nationality= "North America"
+if nationality=="North America" and age>=35:
+    print("You can be President")
+else:
+    print("You cannot President")
+
+constituency="Kasarani"
+if constituency=="Kasarani" or "Embakasi" or "Westlands":
+    print("you can be a governor")
+else:
+    print("you can't be a governor")
